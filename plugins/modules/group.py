094beb80b0460e47f45d4a6824228a48c00d109c
Court <2211329+cocampbe@users.noreply.github.com>
2025-10-09 13:05:26 -0500

    Update group.py
    Reworked the code a bit. Added an example and documentation string. I fixed the error I was getting with multiple regions related to creating the boto3 client.

--- plugins/modules/group.py
+++ plugins/modules/group.py
@@ -4,87 +4,147 @@
 # Copyright: (c) 2025, Neuy Mich <begoingto.me@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
+DOCUMENTATION = r"""
+---
+module: idc_group
+version_added_collection: community.aws
+short_description: Manage AWS Identity Center groups
+description:
+  - Manage AWS Identity Center groups.
+author:
+  - Neuy Mich (@begoingto)
+  - Courtney Campbell (@cocampbe)
+options:
+  name:
+    description:
+      - The name of the group.
+    required: true
+    type: str
+  description:
+    description:
+      - A description of the group.
+    required: false
+    type: str
+  identity_store_id:
+    description:
+      - AWS identity store ID
+    required: true
+    type: str
+  state:
+    description:
+      - Create or remove the IDC group.
+    required: true
+    choices: [ 'present', 'absent' ]
+    type: str
+extends_documentation_fragment:
+  - amazon.aws.common.modules
+  - amazon.aws.region.modules
+  - amazon.aws.boto3
+"""
+
+EXAMPLES = r"""
+# Note: These examples do not set authentication details, see the AWS Guide for details.
+
+- name: Create a group
+  community.aws.idc_group:
+    name: testgroup1
+    state: present
+
+- name: Delete the group
+  community.aws.idc_group:
+    name: testgroup1
+    state: absent
+"""
+
+
+from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 from botocore.exceptions import ClientError
 
-def run_module():
-    module_args = dict(
-        identity_store_id=dict(type='str', required=True),
-        display_name=dict(type='str', required=True),
-        description=dict(type='str', required=False, default=''),
-        state=dict(type='str', default='present', choices=['present', 'absent']),
-        region=dict(type='str', required=False, default=None)
+def create_group(connection, module):
+    display_name = module.params['name']
+    description = module.params['description']
+    identity_store_id = module.params['identity_store_id']
+
+    existing_groups = get_idc_group(connection, module)
+
+    if existing_groups:
+        module.exit_json(changed=False, idc_group=display_name)
+    else:
+        if description == None:
+            response = connection.create_group(
+                IdentityStoreId=identity_store_id,
+                DisplayName=display_name
+            )
+        else:
+            response = connection.create_group(
+                IdentityStoreId=identity_store_id,
+                DisplayName=display_name,
+                Description=description
+            )
+
+        module.exit_json(changed=True, idc_group=display_name)
+
+    if module.check_mode:
+        module.exit_json(changed=True, idc_group=display_name)
+
+
+def destroy_group(connection, module):
+    display_name = module.params['name']
+    identity_store_id = module.params['identity_store_id']
+
+    existing_groups = get_idc_group(connection, module)
+
+    if existing_groups:
+        connection.delete_group(
+            IdentityStoreId=identity_store_id,
+            GroupId=existing_groups[0]['GroupId']
+        )
+
+        module.exit_json(changed=True, idc_group=display_name)
+    else:
+        module.exit_json(changed=False, idc_group=display_name)
+
+    if module.check_mode:
+        module.exit_json(changed=True, idc_group=display_name)
+
+def get_idc_group(connection, module):
+    display_name = module.params['name']
+    identity_store_id = module.params['identity_store_id']
+
+    response = connection.list_groups(
+        IdentityStoreId=identity_store_id,
+        Filters=[{'AttributePath': 'DisplayName', 'AttributeValue': display_name}]
     )
 
-    result = dict(
-        changed=False,
-        group_id='',
-        message=''
+    return response.get('Groups', [])
+
+
+def main():
+    argument_spec = dict(
+        identity_store_id=dict(type='str', required=True),
+        name=dict(type='str', required=True),
+        description=dict(type='str', required=False, default=None),
+        region=dict(type='str', required=True),
+        state=dict(choices=['present', 'absent'], required=True),
     )
 
     module = AnsibleAWSModule(
-        argument_spec=module_args,
+        argument_spec=argument_spec,
         supports_check_mode=True
     )
 
-    identity_store_id = module.params['identity_store_id']
-    display_name = module.params['display_name']
-    description = module.params['description']
     state = module.params['state']
-    region = module.params['region']
+
+    connection = module.client("identitystore")
 
     try:
-        # Initialize identitystore client using AnsibleAWSModule's boto3 client
-        client = module.client('identitystore', region_name=region)
-
-        # Check if group exists
-        response = client.list_groups(
-            IdentityStoreId=identity_store_id,
-            Filters=[{'AttributePath': 'DisplayName', 'AttributeValue': display_name}]
-        )
-        existing_groups = response.get('Groups', [])
-
         if state == 'present':
-            if existing_groups:
-                result['group_id'] = existing_groups[0]['GroupId']
-                result['message'] = f"Group {display_name} already exists"
-            else:
-                if not module.check_mode:
-                    response = client.create_group(
-                        IdentityStoreId=identity_store_id,
-                        DisplayName=display_name,
-                        Description=description
-                    )
-                    result['group_id'] = response['GroupId']
-                    result['changed'] = True
-                    result['message'] = f"Group {display_name} created successfully"
-                else:
-                    result['changed'] = True
-                    result['message'] = f"Group {display_name} would be created (check mode)"
-
-        elif state == 'absent':
-            if existing_groups:
-                if not module.check_mode:
-                    client.delete_group(
-                        IdentityStoreId=identity_store_id,
-                        GroupId=existing_groups[0]['GroupId']
-                    )
-                    result['changed'] = True
-                    result['message'] = f"Group {display_name} deleted successfully"
-                else:
-                    result['changed'] = True
-                    result['message'] = f"Group {display_name} would be deleted (check mode)"
-            else:
-                result['message'] = f"Group {display_name} does not exist"
-
+            create_group(connection, module)
+        else:
+            destroy_group(connection, module)
     except ClientError as e:
-        module.fail_json_aws(e, msg=f"AWS API error: {str(e)}")
+        module.fail_json_aws(e)
 
-    module.exit_json(**result)
-
-def main():
-    run_module()
 
 if __name__ == '__main__':
-
     main()
